c479cb1d099c2ee866fd016cd787d57ba5b72166
Giles Lavelle <giles.lavelle@gmail.com>
2014-03-10 11:48:46 +0000

    new api

--- src/main.js
+++ src/main.js
@@ -21,15 +21,8 @@
         }
     };
 
-    window.fl = window.fl || {};
-
-    // URL of the server. Comment for development/production
-    // George server
-    window.fl.server = 'http://nl.ks07.co.uk:5000/';
-    // AWS
-    // window.fl.server = 'http://ec2-54-194-186-121.eu-west-1.compute.amazonaws.com/';
-    // Local
-    // window.fl.server = 'http://localhost:5000/';
+    // Library root object
+    var api = {};
 
     /**
      * Base method from which all others extend. Sends an AJAX request to
@@ -37,9 +30,9 @@
      * as any defaults not provided. Uses jQuery's `ajax` method for handling
      * the request to normalise cross-browser inconsistencies
      */
-    window.fl.ajax = function(options) {
+    api.ajax = function(options) {
         options = $.extend(defaults, options);
-        options.url = fl.server + options.url;
+        options.url = this.url + options.url;
 
         if (debug){
             console.log(options);
@@ -51,7 +44,7 @@
     /**
      * Sends a HTTP GET request with all supplied options.
      */
-    window.fl.get = function(url, success) {
+    api.get = function(url, success) {
         var options = {
             type: 'GET',
             url: url,
@@ -63,7 +56,7 @@
     /**
      * Sends a HTTP POST request with all supplied options.
      */
-    window.fl.post = function(url, data, success) {
+    api.post = function(url, data, success) {
         var options = {
             type: 'POST',
             url: url,
@@ -73,18 +66,18 @@
         fl.ajax(options);
     };
 
-    window.fl.login = function(data, success) {
+    api.login = function(data, success) {
         fl.post('login', data, success);
     };
 
-    window.fl.register = function(data, success) {
+    api.register = function(data, success) {
         fl.post('register', data, success);
     };
 
     /**
      * Uploads the given image metadata to the server.
      */
-    window.fl.uploadMetadata = function(data) {
+    api.uploadMetadata = function(data) {
         fl.post('upload/metadata', data);
     };
 
@@ -94,7 +87,7 @@
      * @param success {Function} The callback to be executed when the data loads.
      * @param project {Integer} The ID of the project to load features of.
      */
-    window.fl.getFeatures = function(success, project) {
+    api.getFeatures = function(success, project) {
         // TODO: don't hardcode
         project = 1;
         var url = 'project/fields?project=' + project;
@@ -104,7 +97,7 @@
     /**
      * Gets the list of all species currently available for annotation.
      */
-    window.fl.getSpecies = function(success) {
+    api.getSpecies = function(success) {
         fl.get('projects', success);
     };
 
@@ -112,25 +105,30 @@
      * Determines the authorization status of the current session, and returns
      * the user's information if the check passes.
      */
-    window.fl.loginCheck = function(success) {
+    api.loginCheck = function(success) {
         fl.get('logincheck', success);
     };
 
     /**
      * Terminates the current session.
      */
-    window.fl.logout = function(success) {
+    api.logout = function(success) {
         fl.get('logout', success);
     };
 
     /**
      * Gets a bunch of images or something not really sure here guys.
      */
-    window.fl.getImages = function(success) {
+    api.getImages = function(success) {
         fl.get('images', success);
     };
 
-    window.fl.getJobs = function(success) {
+    api.getJobs = function(success) {
         fl.get('jobs', success);
+    };
+
+    window.fl_api = function(url) {
+        api.url = url || 'http://ec2-54-194-186-121.eu-west-1.compute.amazonaws.com/';
+        return api;
     };
 })();
